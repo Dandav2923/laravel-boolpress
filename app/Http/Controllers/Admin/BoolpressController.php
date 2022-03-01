94d3ba795494a886c8cf632e7f2525ccef3231c3
Dandav2923 <daniele.emili23@gmail.com>
2022-03-01 15:04:03 +0100

    modifica istanza

--- app/Http/Controllers/Admin/BoolpressController.php
+++ app/Http/Controllers/Admin/BoolpressController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Boolpress;
-class PostController extends Controller
+class BoolpressController extends Controller
 {
     /**
      * Show the application dashboard.
