ca60146edfe0d22524dfc56cb41e01e87b6dac98
Dandav2923 <daniele.emili23@gmail.com>
2022-03-11 17:59:48 +0100

    stampa post in home

--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -2,7 +2,7 @@
   <div class="container">
       <div class="row row-cols-3">
           <div class="col" 
-            v-for="(post , index) in postArray" :key="index" >
+            v-for="(post , index) in cards.postArray" :key="index" >
             <div class="card">
               <h4>{{ post.title }}</h4>
               <p>{{ post.content }}</p>
@@ -13,28 +13,10 @@
 </template>
 
 <script>
-import Axios from 'axios';
+
 export default {
   name : "Main",
-  data() {
-        return{
-            postArray: null,
-            next_page_url: null,
-            prev_page_url: null
-        }
-    },
-    created() {
-      const vm = this;
-      Axios.get('http://127.0.0.1:8000/api/posts')
-      .then(results => {
-        vm.postArray = results.data.results.data;
-        vm.next_page_url = results.data.next_page_url;
-        vm.prev_page_url = results.data.prev_page_url;
-      })
-      .catch(error => {
-        console.log(error);
-      })
-    }
+  props: ['cards']
 }
 </script>
 
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,17 +1,46 @@
 <template>
-  <div class="container">
-      <div class="row">
-          <div class="col">Home</div>
+  <div class="container-fluid">
+      <div class="row"> 
+          <div class="col">
+            <h1>Home</h1>
+          </div>
+          <Main :cards= "cards"/>
       </div>
   </div>
 </template>
 
 <script>
+import Axios from 'axios';
+import Main from '../components/Main.vue';
 export default {
-name : "Home",
+  name : "Home",
+  components: {
+    Main
+  },
+  data() {
+        return{ 
+          cards:{
+            postArray: null,
+            next_page_url: null,
+            prev_page_url: null
+          }
+        }
+    },
+    created() {
+      const vm = this;
+      Axios.get('http://127.0.0.1:8000/api/posts')
+      .then(results => {
+        vm.cards.postArray = results.data.results.data;
+        vm.cards.next_page_url = results.data.next_page_url;
+        vm.cards.prev_page_url = results.data.prev_page_url;
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
 </style>
